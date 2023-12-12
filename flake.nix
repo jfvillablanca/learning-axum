fc17b95c07f62b0b50e84e99836b643c35f782c2
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-12-12 21:09:19 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  outputs = { self, nixpkgs, devenv, flake-utils, ... } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        cargo-pretty-test = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "cargo-pretty-test";
+          version = "v0.2.3";
+          src = pkgs.fetchFromGitHub {
+            owner = "josecelano";
+            repo = "cargo-pretty-test";
+            rev = "main";
+            hash = "sha256-VnnhSgvNfqXLKTYe+Sef9H80+Ym7BBo7Jnfd2eMWF4U=";
+          };
+          cargoLock = {
+            lockFile = src + "/Cargo.lock";
+          };
+          doCheck = false;
+        };
+      in
+      {
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              languages = {
+                rust.enable = true;
+              };
+              packages = [
+                cargo-pretty-test
+              ] ++ (with pkgs; [
+                cargo-watch
+              ]);
+            }
+          ];
+        };
+      });
+}
